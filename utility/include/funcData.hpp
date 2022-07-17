1b4eb23fa1ce1eb53d54c214a4154646dc40128a
ExactlyWb <frolov.da@phystech.edu>
2022-07-17 20:49:24 +0300

    Start C3 original algorithm refactoring

--- utility/include/funcData.hpp
+++ utility/include/funcData.hpp
@@ -1,7 +1,61 @@
 #ifndef _FUNC_DATA_HPP__
 #define _FUNC_DATA_HPP__
 
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <stdint.h>
+
+#include <boost/heap/fibonacci_heap.hpp>
+
 namespace HFData {
+
+const int C3_CLUSTER_THRESHOLD = 1024;
+
+struct node {
+
+    std::string name_;
+    std::size_t size_;
+
+};
+
+struct cluster_edge;
+
+struct cluster
+{
+    cluster (node *node, int size, std::size_t freq, std::size_t miss):
+        m_functions (), m_callers (), m_size (size), m_freq (freq), m_miss (miss)
+    {
+        m_functions.push_back (node);
+    }
+
+    std::vector <node*> m_functions;
+    
+    std::unordered_map <cluster *, cluster_edge *> m_callers;
+    int m_size;
+
+    std::size_t m_freq;
+    std::size_t m_miss;
+};
+
+/* Cluster edge is an oriented edge in between two clusters.  */
+
+struct cluster_edge
+{
+    cluster_edge (cluster *caller, cluster *callee, uint32_t count):
+        m_caller (caller), m_callee (callee), m_count (count), m_heap_node (NULL)
+    {}
+
+    uint32_t inverted_count ()
+    {
+        return UINT32_MAX - m_count;
+    }
+
+    cluster *m_caller;
+    cluster *m_callee;
+    uint32_t m_count;
+    boost::heap::fibonacci_heap<uint32_t, cluster_edge> *m_heap_node;
+};
 
 }
 
--- utility/source/algorithm.cpp
+++ utility/source/algorithm.cpp
@@ -8,15 +8,10 @@ namespace FunctionReordering {
             tlbMissesSamples_,
             cyclesSample_,
             perfPath_);  //! TODO check perf file for event + tlbMisses
-        // fillTbl (perfFuncTbl_, lbrSamples_.begin (), lbrSamples_.end ());
         nmParser::parse_nm_data (nmFunctions_, nmPath_);
 
-        std::cerr << tlbMissesSamples_.size () << std::endl;
-        std::cerr << cyclesSample_.size () << std::endl;
 
-        for (auto v : nmFunctions_)
-            std::cerr << v.name_ << ", size = " << v.size_ << std::endl;
-        //! TODO
+        
     }
 
 }  // namespace FunctionReordering
